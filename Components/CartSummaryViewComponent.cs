52188542d9aebd09aba4bafabd4a98c555b7ccae
spencerrfree <sf376@byu.edu>
2023-03-10 14:06:34 -0700

    Mission_11_Done

---
+++ Components/CartSummaryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sf376_Mission_9.Models;
+
+namespace Sf376_Mission_9.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Cart cart;
+        public CartSummaryViewComponent(Cart cartService)
+        {
+            cart = cartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
